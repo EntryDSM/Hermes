839542a2ed758d55babebb6b1b4c4331db4635b4
Lewis <artoria@artoria.us>
2019-03-11 11:37:32 +0900

    UPDATE: 라우팅 및 뷰 클래스 정의

--- user/router.py
+++ user/router.py
@@ -1,8 +1,13 @@
 from sanic import Blueprint
 
-from user.view import User, Status
+from user.view import (UserView, UserInfoView, UserStatusView, UserBatchView, AdminView, AdminInfoView, AdminBatchView)
 
-bp = Blueprint("user")
+bp = Blueprint("user", url_prefix="/api/v1")
 
-bp.add_route(User.as_view(), "/user")
-bp.add_route(Status.as_view(), "/user/status")
+bp.add_route(UserView.as_view(), "/user")
+bp.add_route(UserInfoView.as_view(), "/user/<user_email>")
+bp.add_route(UserBatchView.as_view(), "/user/batch")
+bp.add_route(UserStatusView.as_view(), "/user/status")
+bp.add_route(AdminView.as_view(), "/admin")
+bp.add_route(AdminInfoView.as_view(), "/admin/<admin_id>")
+bp.add_route(AdminBatchView.as_view(), "/admin/batch")
