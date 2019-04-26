fb87a994ad2b57bad4514d07c9e2983ba711e7f8
Lewis <artoria@artoria.us>
2019-04-26 19:57:48 +0900

    ADD: 어댑터 친구들을 위한 ABC(인터페이스) 제작

---
+++ hermes/adapters/__init__.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractAdapter(ABC):
+    @abstractmethod
+    def _data_to_entity(self, schema, data): ...
+
+    @abstractmethod
+    def _entity_to_data(self, schema, entity): ...
