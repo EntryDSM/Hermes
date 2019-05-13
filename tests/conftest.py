8af61a81a9eb29c36e4e786ee872f4915af595ac
Lewis <artoria@artoria.us>
2019-05-13 11:22:48 +0900

    UPDATE: pytest-sanic을 사용

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
 # pylint: disable=unused-argument,redefined-outer-name
+from dataclasses import asdict
+
 import pytest
+from sanic.websocket import WebSocketProtocol
 
 from hermes.presentation.app import create_app
+from hermes.repositories.admin import (AdminCacheRepository,
+                                       AdminPersistentRepository)
 from hermes.repositories.connections import MySQLConnection, RedisConnection
+from tests.helpers import (create_admin_dummy_object, create_admin_table,
+                           save_admins)
 
 
 @pytest.fixture(scope="function")
@@ -35,7 +42,7 @@ async def cache_manage(redis_proc):
     await RedisConnection.destroy()
 
 
-@pytest.fixture(scope="function")
+@pytest.yield_fixture(scope="function")
 async def hermes_app(mysql_proc, mysql, redis_proc):
     _app = create_app()
 
@@ -46,15 +53,19 @@ async def hermes_app(mysql_proc, mysql, redis_proc):
         "db": "pytest_mysql_db",
         "host": mysql_proc.host,
         "port": mysql_proc.port,
-        "loop": None,
         "autocommit": True,
     }
 
     _app.cache_connection_info = {
         "address": f"redis://:@{redis_proc.host}:{redis_proc.port}",
         "minsize": 5,
-        "maxsize": 10,
     }
 
-    return _app.test_client
+    yield _app
+
+
+@pytest.fixture(scope="function")
+def test_cli(loop, hermes_app, sanic_client):
+    return loop.run_until_complete(sanic_client(hermes_app, protocol=WebSocketProtocol))
+
 
