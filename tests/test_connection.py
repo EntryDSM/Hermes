7b273478a58c7153f286bc625610a8a73e490643
Seonghyeon Kim <artoria@artoria.us>
2019-05-14 14:33:22 +0900

    ADD: cache connection test function

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -15,3 +15,21 @@ async def test_db_connection_interface(db_method, mysql_manage):
             False
         ), f"expected success, but error: '{e}' occurred while testing method '{db_method}'."
 
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "cache_method, param",
+    [
+        ("set", {"key": "x", "value": {"y": 0}}),
+        ("get", {"key": "x"}),
+        ("delete", {"key": "x"}),
+        ("flush_all", {})
+    ],
+)
+async def test_cache_connection_interface(cache_method, param, cache_manage):
+    try:
+        await getattr(RedisConnection, cache_method)(**param)
+    except RedisError as e:
+        assert (
+            False
+        ), f"expected success, but error: '{e}' occurred while testing method '{cache_method}'."
