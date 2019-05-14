94ae9402a9b689974664dd7630aafd2a5875043e
Seonghyeon Kim <artoria@artoria.us>
2019-05-14 17:08:58 +0900

    ADD: persistent repo get test

--- tests/test_repositories.py
+++ tests/test_repositories.py
@@ -23,3 +23,19 @@ async def test_admin_persistent_repo_patch(
 
     await repo.patch(generate_admin_id(0), {"admin_type": "INTERVIEW"})
 
+
+@pytest.mark.asyncio
+async def test_admin_persistent_repo_get(
+    mysql_manage, admin_persistent_repo, save_admin_dummy_to_db
+):
+    repo = admin_persistent_repo
+
+    result = await repo.get_one(generate_admin_id(0))
+    assert generate_admin_id(0) == result["admin_id"]
+
+    result = await repo.get_list()
+    assert isinstance(result, list) and isinstance(result[0], dict)
+
+    result = await repo.get_list({"admin_type": "ROOT"})
+    assert isinstance(result, list) and generate_admin_id(0) == result[0]["admin_id"]
+
