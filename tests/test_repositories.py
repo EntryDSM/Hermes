ea5afa68d111721f72cfe0b90bb68d65f68a0efb
Seonghyeon Kim <artoria@artoria.us>
2019-05-14 17:08:40 +0900

    ADD: persistent repo patch test

--- tests/test_repositories.py
+++ tests/test_repositories.py
@@ -14,3 +14,12 @@ async def test_admin_persistent_repo_save(
 
     await repo.save(test_data)
 
+
+@pytest.mark.asyncio
+async def test_admin_persistent_repo_patch(
+    mysql_manage, admin_persistent_repo, save_admin_dummy_to_db
+):
+    repo = admin_persistent_repo
+
+    await repo.patch(generate_admin_id(0), {"admin_type": "INTERVIEW"})
+
