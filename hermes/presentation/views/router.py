4420cb77ff8a89cf6166dc6fe4896b24ef2b49aa
Seonghyeon Kim <artoria@artoria.us>
2019-06-28 16:21:03 +0900

    ADD: admin authorization view

--- hermes/presentation/views/router.py
+++ hermes/presentation/views/router.py
@@ -1,6 +1,11 @@
 from sanic import Blueprint
 
-from hermes.presentation.views.admin import AdminBatchView, AdminDetailView, AdminView
+from hermes.presentation.views.admin import (
+    AdminView,
+    AdminBatchView,
+    AdminDetailView,
+    AdminAuthorizationView,
+)
 from hermes.presentation.views.applicant import (
     ApplicantBatchView,
     ApplicantDetailView,
@@ -17,5 +22,6 @@ bp.add_route(ApplicantDetailView.as_view(), "/applicant/<email>")
 bp.add_route(ApplicantAuthorizationView.as_view(), '/applicant/<email>/authorization')
 bp.add_route(ApplicantStatusView.as_view(), "/applicant/<email>/status")
 bp.add_route(AdminView.as_view(), "/admin")
-bp.add_route(AdminDetailView.as_view(), "/admin/<admin_id>")
 bp.add_route(AdminBatchView.as_view(), "/admin/batch")
+bp.add_route(AdminDetailView.as_view(), "/admin/<admin_id>")
+bp.add_route(AdminAuthorizationView.as_view(), "/admin/<admin_id>/authorization")
